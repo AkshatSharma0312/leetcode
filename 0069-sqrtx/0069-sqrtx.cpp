4299091cf98c203142004f56e8d4ff1222db4fc3
AkshatSharma0312 <akshatsharma4964@gmail.com>
2026-07-21 23:55:48 +0530

    Time: 6 ms (14.42%), Space: 8.7 MB (14.5%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int u=pow(x,0.5);
-        return u;
+        if(x == 0)
+            return 0;
+        else if(x == 1)
+            return 1;
+
+        for(long int i = 1; i < x/2 + 2; i++) {
+            if(i * i > x)
+                return i - 1;
+        }
+
+        return -1;
     }
 };
