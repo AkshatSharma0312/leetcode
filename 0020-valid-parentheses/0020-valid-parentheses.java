123cb68977d28eeadc2effefca3ac05451002f2a
AkshatSharma0312 <akshatsharma4964@gmail.com>
2026-09-13 22:45:13 +0530

    Time: 1 ms (99.94%), Space: 43.4 MB (40.8%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        boolean changed = true;
+        char[] arr = new char[s.length()];
+        int top = -1;
 
-        while (changed && !s.isEmpty()) {
-            changed = false;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                arr[++top] = c;
+            } else {
+                if (top == -1) return false;
 
-            for (int i = 0; i < s.length() - 1; i++) {
-                String pair = s.substring(i, i + 2);
-
-                if (pair.equals("()") ||
-                    pair.equals("{}") ||
-                    pair.equals("[]")) {
-
-                    s = s.substring(0, i) + s.substring(i + 2);
-                    changed = true;
-                    break;
+                if ((c == ')' && arr[top] != '(') ||
+                    (c == '}' && arr[top] != '{') ||
+                    (c == ']' && arr[top] != '[')) {
+                    return false;
                 }
+
+                top--;
             }
         }
 
-        return s.isEmpty();
+        return top == -1;
     }
 }
